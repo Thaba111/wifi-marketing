83a96128ed8b23aab2849080918ba95dab14f1cd
fabianmuema <muemafabian@gmail.com>
2024-09-27 08:29:21 +0300

    remove the role table column

--- database/migrations/2024_09_21_141058_create_users_table.php
+++ database/migrations/2024_09_21_141058_create_users_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();  // Auto-increment ID
-            $table->string('name');  
-            $table->string('email')->unique();  
-            $table->string('password');  
-            $table->enum('role', ['admin', 'marketer', 'viewer']);  
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->boolean('status')->default(true);  // Active or inactive status
-            $table->timestamps();  
+            $table->timestamps();
         });
     }
 
